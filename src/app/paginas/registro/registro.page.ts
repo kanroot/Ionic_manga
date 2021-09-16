7813e636306fe5bad01fb001bfe76c640a48123b
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-09-15 21:44:22 -0300

    4 espacios por tab

--- src/app/paginas/registro/registro.page.ts
+++ src/app/paginas/registro/registro.page.ts
@@ -2,40 +2,42 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {IonSlides} from '@ionic/angular';
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.page.html',
-  styleUrls: ['./registro.page.scss'],
+    selector: 'app-registro',
+    templateUrl: './registro.page.html',
+    styleUrls: ['./registro.page.scss'],
 })
 
 export class RegistroPage implements OnInit {
-  @ViewChild('avatarId')
-  avatarId: IonSlides;
-  usuario ={
-    nick:'',
-    avatar: 0,
-    email:'',
-    password:'',
-    password2:''
-  };
+    @ViewChild('avatarId')
+    avatarId: IonSlides;
+    usuario = {
+        nick: '',
+        avatar: 0,
+        email: '',
+        password: '',
+        password2: ''
+    };
 
-  slideOpts = {
-    initialSlide: 0,
-    speed: 400,
-    pager:true,
-    scrollbar:true
-  };
+    slideOpts = {
+        initialSlide: 0,
+        speed: 400,
+        pager: true,
+        scrollbar: true
+    };
 
-  constructor() {}
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-  onSubmit(){
-    console.log('submit');
-    console.log(this.usuario);
-  }
+    ngOnInit() {
+    }
 
-  async seleccionarAvatar(){
-    this.usuario.avatar =  await this.avatarId.getActiveIndex();
-  }
+    onSubmit() {
+        console.log('submit');
+        console.log(this.usuario);
+    }
+
+    async seleccionarAvatar() {
+        this.usuario.avatar = await this.avatarId.getActiveIndex();
+    }
 
 }
