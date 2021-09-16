b577a40317931eb3739064da33bd823015686227
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-09-15 21:43:55 -0300

    agregada información a preview de mangas

--- src/app/paginas/catalogo/catalogo.page.ts
+++ src/app/paginas/catalogo/catalogo.page.ts
@@ -1,94 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MangaPreviewModel} from '../../componentes-compartidos/mangapreview/mangapreview.component';
 
 @Component({
-  selector: 'app-catalogo',
-  templateUrl: './catalogo.page.html',
-  styleUrls: ['./catalogo.page.scss'],
+    selector: 'app-catalogo',
+    templateUrl: './catalogo.page.html',
+    styleUrls: ['./catalogo.page.scss'],
 })
 export class CatalogoPage implements OnInit {
-  //TODO: tomar desde servicio en lugar hardcodear
-  catalogo: MangaPreviewModel[] = [
-    {
-      id: 0,
-      nombre: 'Manga 1',
-      rutaImg: 'assets/img/devil.jpg'
-    },
-    {
-      id: 1,
-      nombre: 'Manga 2',
-      rutaImg: 'assets/img/hitomi.jpg'
-    },
-    {
-      id: 2,
-      nombre: 'Manga 3',
-      rutaImg: 'assets/img/berserk.jpg'
-    },
-    {
-      id: 3,
-      nombre: 'Manga 4',
-      rutaImg: 'assets/img/devil.jpg'
-    },
-    {
-      id: 4,
-      nombre: 'Manga 5',
-      rutaImg: 'assets/img/hitomi.jpg'
-    },
-    {
-      id: 0,
-      nombre: 'Manga 1',
-      rutaImg: 'assets/img/berserk.jpg'
-    },
-    {
-      id: 1,
-      nombre: 'Manga 2',
-      rutaImg: 'assets/img/devil.jpg'
-    },
-    {
-      id: 2,
-      nombre: 'Manga 3',
-      rutaImg: 'assets/img/hitomi.jpg'
-    },
-    {
-      id: 3,
-      nombre: 'Manga 4',
-      rutaImg: 'assets/img/berserk.jpg'
-    },
-    {
-      id: 4,
-      nombre: 'Manga 5',
-      rutaImg: 'assets/img/devil.jpg'
-    },
-    {
-      id: 0,
-      nombre: 'Manga 1',
-      rutaImg: 'assets/img/hitomi.jpg'
-    },
-    {
-      id: 1,
-      nombre: 'Manga 2',
-      rutaImg: 'assets/img/berserk.jpg'
-    },
-    {
-      id: 2,
-      nombre: 'Manga 3',
-      rutaImg: 'assets/img/devil.jpg'
-    },
-    {
-      id: 3,
-      nombre: 'Manga 4',
-      rutaImg: 'assets/img/hitomi.jpg'
-    },
-    {
-      id: 4,
-      nombre: 'Manga 5',
-      rutaImg: 'assets/img/berserk.jpg'
-    },
-  ];
+    //TODO: tomar desde servicio en lugar hardcodear
+    catalogo: MangaPreviewModel[] = [
+        {
+            id: 0,
+            nombre: 'Devilman',
+            rutaImg: 'assets/img/devil.jpg'
+        },
+        {
+            id: 1,
+            nombre: 'Domestic na kanojo',
+            rutaImg: 'assets/img/hitomi.png'
+        },
+        {
+            id: 2,
+            nombre: 'Berserk',
+            rutaImg: 'assets/img/berserk.jpg'
+        },
+        {
+            id: 0,
+            nombre: 'Devilman',
+            rutaImg: 'assets/img/devil.jpg'
+        },
+        {
+            id: 1,
+            nombre: 'Domestic na kanojo',
+            rutaImg: 'assets/img/hitomi.png'
+        },
+        {
+            id: 2,
+            nombre: 'Berserk',
+            rutaImg: 'assets/img/berserk.jpg'
+        },
+        {
+            id: 0,
+            nombre: 'Devilman',
+            rutaImg: 'assets/img/devil.jpg'
+        },
+        {
+            id: 1,
+            nombre: 'Domestic na kanojo',
+            rutaImg: 'assets/img/hitomi.png'
+        },
+        {
+            id: 2,
+            nombre: 'Berserk',
+            rutaImg: 'assets/img/berserk.jpg'
+        },
+        {
+            id: 0,
+            nombre: 'Devilman',
+            rutaImg: 'assets/img/devil.jpg'
+        },
+        {
+            id: 1,
+            nombre: 'Domestic na kanojo',
+            rutaImg: 'assets/img/hitomi.png'
+        },
+        {
+            id: 2,
+            nombre: 'Berserk',
+            rutaImg: 'assets/img/berserk.jpg'
+        },
+    ];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
