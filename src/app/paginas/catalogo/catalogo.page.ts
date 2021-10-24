821791b652a270e89b3f5e3f73b97026161a69df
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-10-24 17:06:00 -0300

    feat: primera interación implementación de grape-kun-api

--- src/app/paginas/catalogo/catalogo.page.ts
+++ src/app/paginas/catalogo/catalogo.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MangaPreviewModel} from '../../compartido/modelos/manga-preview.model';
+import {GrapeKunApiService} from '../../servicios/grape-kun-api.service';
 
 @Component({
     selector: 'app-catalogo',
@@ -7,158 +8,14 @@ import {MangaPreviewModel} from '../../compartido/modelos/manga-preview.model';
     styleUrls: ['./catalogo.page.scss'],
 })
 export class CatalogoPage implements OnInit {
-    //TODO: tomar desde servicio en lugar hardcodear
-    catalogo: MangaPreviewModel[] = [
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Devilman',
-                enlaceImg: 'assets/img/devil.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Domestic no kanojo',
-                enlaceImg: 'assets/img/hitomi.png',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Berserk',
-                enlaceImg: 'assets/img/berserk.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Devilman',
-                enlaceImg: 'assets/img/devil.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Domestic no kanojo',
-                enlaceImg: 'assets/img/hitomi.png',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Berserk',
-                enlaceImg: 'assets/img/berserk.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Devilman',
-                enlaceImg: 'assets/img/devil.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Domestic no kanojo',
-                enlaceImg: 'assets/img/hitomi.png',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Berserk',
-                enlaceImg: 'assets/img/berserk.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Devilman',
-                enlaceImg: 'assets/img/devil.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Domestic no kanojo',
-                enlaceImg: 'assets/img/hitomi.png',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Berserk',
-                enlaceImg: 'assets/img/berserk.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Devilman',
-                enlaceImg: 'assets/img/devil.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Domestic no kanojo',
-                enlaceImg: 'assets/img/hitomi.png',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Berserk',
-                enlaceImg: 'assets/img/berserk.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Devilman',
-                enlaceImg: 'assets/img/devil.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Domestic no kanojo',
-                enlaceImg: 'assets/img/hitomi.png',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-        new MangaPreviewModel().deserializar(
-            {
-                nombre: 'Berserk',
-                enlaceImg: 'assets/img/berserk.jpg',
-                enlaceManga: 'https://google.cl',
-                generos: ['género 1', 'género 2']
-            }
-        ),
-    ];
+    catalogo: MangaPreviewModel[] = [];
 
-    constructor() {
+    constructor(private api: GrapeKunApiService) {
     }
 
     ngOnInit() {
+        this.api.consultarCatalogo().subscribe(resp => {
+            this.catalogo.push(...resp.results);
+        });
     }
-
 }
---
+++ src/app/servicios/grape-kun-api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {RespuestaCatalogo} from '../compartido/modelos/manga-preview.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GrapeKunApiService {
+
+    constructor(private http: HttpClient ) { }
+
+    consultarCatalogo()
+    {
+        return this.http.get<RespuestaCatalogo>(environment.urlCatalogo);
+    }
+}
