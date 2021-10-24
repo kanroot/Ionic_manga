b77a4583e2a01f2a170f1a18f0ad012af3adec57
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-10-24 17:57:16 -0300

    feat: agregado scroll infinito al catalogo

--- src/app/paginas/catalogo/catalogo.page.ts
+++ src/app/paginas/catalogo/catalogo.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {MangaPreviewModel} from '../../compartido/modelos/manga-preview.model';
+import {MangaPreviewModel, RespuestaCatalogo} from '../../compartido/modelos/manga-preview.model';
 import {GrapeKunApiService} from '../../servicios/grape-kun-api.service';
 
 @Component({
@@ -8,14 +8,34 @@ import {GrapeKunApiService} from '../../servicios/grape-kun-api.service';
     styleUrls: ['./catalogo.page.scss'],
 })
 export class CatalogoPage implements OnInit {
+    paginaActual = 1;
     catalogo: MangaPreviewModel[] = [];
+    resp: RespuestaCatalogo;
 
     constructor(private api: GrapeKunApiService) {
     }
 
     ngOnInit() {
-        this.api.consultarCatalogo().subscribe(resp => {
+        this.consultarCatalogoPorPagina(1);
+    }
+
+    consultarCatalogoPorPagina(pagina: number) {
+        this.api.consultarCatalogo(pagina).subscribe(resp => {
+            this.resp = resp;
             this.catalogo.push(...resp.results);
         });
+    }
+
+    cargarMasMangas(infiniteScroll) {
+        console.log(infiniteScroll);
+        if (this.resp.next != null) {
+            this.paginaActual += 1;
+            setTimeout(
+                () => {
+                    this.consultarCatalogoPorPagina(this.paginaActual);
+                }, 500
+            );
+        }
+        infiniteScroll.target.complete();
     }
 }
