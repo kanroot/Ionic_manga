4b3574b366de3c7b2de9f2ff182ad906a53a3f7b
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-10-24 17:06:37 -0300

    fix: corrección modelo manga preview

--- src/app/compartido/modelos/manga-preview.model.ts
+++ src/app/compartido/modelos/manga-preview.model.ts
@@ -1,12 +1,23 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import {IDeserializable} from './ideserializable';
+
+export class RespuestaCatalogo {
+    count: number;
+    next: string;
+    previous: string;
+    results: MangaPreviewModel[];
+}
 
 /* Representación de un manga en su forma más básica, útil para armar catálogo.
 * */
 export class MangaPreviewModel implements IDeserializable<MangaPreviewModel>{
+    id: number;
     nombre: string;
-    enlaceImg: string;
-    enlaceManga: string;
-    generos: string[];
+    enlace_img: string;
+    enlace_manga: string;
+    generos: string;
+    generos_como_lista: string[];
+    contenido_adulto: boolean;
 
     deserializar(input: any): MangaPreviewModel {
         return Object.assign(this, input);
