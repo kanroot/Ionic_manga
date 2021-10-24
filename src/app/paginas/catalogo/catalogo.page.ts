99fd2fe0d7c07e64440922f4e67f0c345c629b8b
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-10-24 17:06:00 -0300

    feat: primera interación implementación de grape-kun-api

--- src/app/paginas/catalogo/catalogo.page.ts
+++ src/app/paginas/catalogo/catalogo.page.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {MangaDetalleModel, MangaPreviewModel, RespuestaCatalogo} from '../../compartido/modelos/manga.modelo';
+import {MangaPreviewModel} from '../../compartido/modelos/manga-preview.model';
 import {GrapeKunApiService} from '../../servicios/grape-kun-api.service';
-import {Router} from '@angular/router';
-import {LoadingController} from '@ionic/angular';
 
 @Component({
     selector: 'app-catalogo',
@@ -10,53 +8,14 @@ import {LoadingController} from '@ionic/angular';
     styleUrls: ['./catalogo.page.scss'],
 })
 export class CatalogoPage implements OnInit {
-    paginaActual = 1;
     catalogo: MangaPreviewModel[] = [];
-    resp: RespuestaCatalogo;
 
-    constructor(private api: GrapeKunApiService, private router: Router, public loadingController: LoadingController) {
+    constructor(private api: GrapeKunApiService) {
     }
 
     ngOnInit() {
-        this.consultarCatalogoPorPagina(1);
-    }
-
-    consultarCatalogoPorPagina(pagina: number) {
-        this.api.consultarCatalogo(pagina).subscribe(resp => {
-            this.resp = resp;
+        this.api.consultarCatalogo().subscribe(resp => {
             this.catalogo.push(...resp.results);
         });
-    }
-
-    cargarMasMangas(infiniteScroll) {
-        if (this.resp.next != null) {
-            this.paginaActual += 1;
-            setTimeout(
-                () => {
-                    this.consultarCatalogoPorPagina(this.paginaActual);
-                }, 500
-            );
-        }
-        infiniteScroll.target.complete();
-    }
-
-    async obtenerDetalleDesdeCatalogo(preview: MangaPreviewModel) {
-        const loading = await this.loadingController.create(
-            {
-                message: 'Espere por favor...',
-                keyboardClose: false
-            }
-        );
-        await loading.present();
-        this.api.consultarMangaDetalle(preview.id).subscribe(
-            resp => {
-                this.navegarAPaginaDetalle(resp);
-                loading.dismiss();
-            }
-        );
-    }
-
-    navegarAPaginaDetalle(detalle: MangaDetalleModel) {
-        this.router.navigate(['detalle', JSON.stringify(detalle)]);
     }
 }
--- src/app/servicios/grape-kun-api.service.ts
+++ src/app/servicios/grape-kun-api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {MangaDetalleModel, RespuestaCatalogo} from '../compartido/modelos/manga.modelo';
-
+import {RespuestaCatalogo} from '../compartido/modelos/manga-preview.model';
 
 @Injectable({
     providedIn: 'root'
@@ -11,11 +10,8 @@ export class GrapeKunApiService {
 
     constructor(private http: HttpClient ) { }
 
-    consultarCatalogo(pagina: number) {
-        return this.http.get<RespuestaCatalogo>(environment.urlCatalogo + `?page=${pagina}`);
-    }
-
-    consultarMangaDetalle(id: number) {
-        return this.http.get<MangaDetalleModel>(environment.urlCatalogo + id);
+    consultarCatalogo()
+    {
+        return this.http.get<RespuestaCatalogo>(environment.urlCatalogo);
     }
 }
